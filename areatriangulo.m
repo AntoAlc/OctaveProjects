0c51b2d2a353a6c737f7c74896cbbd6fa8d71e55
Antonella Alcaraz <antoo.alcaraz@gmail.com>
2019-03-14 19:35:46 -0300

    area

---
+++ areatriangulo.m
@@ -0,0 +1,12 @@
+function areatriangulo  
+  clear;
+  clc;
+  %reales (b, h, A)
+  disp ('ingrese el valor de la base');
+  b=input('');
+  disp('ingrese el valor de la altura:');
+  h=input (' ');
+  A=b*h/2 ;
+  disp('el valor del área es:'); disp (A);
+
+end 
