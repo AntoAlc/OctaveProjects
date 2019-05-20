4583885aa4053350123c8e898529e2ff0d3f98fa
Antonella <antoo.alcaraz@gmail.com>
2019-05-20 18:57:40 -0300

    tarea "terminada"

--- tarea_edo.m
+++ tarea_edo.m
@@ -9,10 +9,12 @@ function tarea_edo
   x(1)=0;
   x(N)=1;
   h=(x(N)-x(1))/(N-1);
-    for i=1:N
+  
+  for i=1:N
     x(i)=h*i;
   end
-  disp('el paso es:'); disp(h);
+  
+  disp('el paso h es:'); disp(h);
   u(1)=0;
   u(N)=0;
   c= zeros(n);
@@ -21,33 +23,89 @@ function tarea_edo
   c(1,2)=1;
   c(n,n-1)=1;
   c(n,n)= -2;
+  
   for i=1:n
     p(i)=h*i;
   end
-  p=p'
+  
+  p=p' %vector paso h
+  
   for i=2:n-1
     c(i,i-1)=1;
     c(i,i)=-2;
     c(i,i+1)=1;
   end
+  
   for i=1:n
     identidad(i,i)=1;
   end
-  c
+    
+  disp('la matriz de coeficientes de edo es '); disp(c);
   identidad
   A=((-1/h^2)*c+identidad)
-  s=inv(A)*p
-  for i=2:N-1
-  u(i)=s(i-1);
+  s=inv(A)*p %vector solucion (puntos interiores)
+  
+  for i=2:N-1 %%agrega al vector u los vectores de s, seria desde la componente 2, hasta la n-1, la 1 y la n las tengo como dato
+    u(i)=s(i-1);
   end
-  disp('el vector u es igual a:'); disp(u);
+  
+  disp('el vector u es igual a:'); disp(u); %muestra el vector solucion con los N puntos
   
   hold on
   plot (x,u, '--b')  %%grafico edo
   
-  %%derivacion
-  
-  %%integracios con trapecios compuesto
+  %%DERIVACION
     
+    for i=2:N
+     x(i)=x(i-1)+h;
+    end
+    
+    coef=zeros(N,N);
+    
+  %derivada primera asimétrica hacia adelante en el primer punto
+  %%asimetrica hacia adelante: (-3*f(i)/2*h)+((2*f(i+1))/h)-((f(i+2))/2*h)
+    coef(1,1)=-3/(2*h);
+    coef(1,2)=2/h;
+    coef(1,3)=-1/(2*h);
+    
+  %derivada primera asimétrica hacia atrás en ultimo punto
+  %%asimetrica hacia atras:  (-3*f(i)/(2*h))-(2*f(i-1)/h)+((f(i-2))/(2*h))
+    coef(N,N)=3/(2*h);
+    coef(N,N-1)=-2/h;
+    coef(N,N-2)=1/(2*h);
   
+  %derivada primera central hacia atrás en el resto de los puntos
+  %%central: (f(i+1)-f(i-1))/(2*h)    
+    for i=2:N-1
+      coef(i,i-1)=-1/(2*h);
+      coef(i,i+1)=1/(2*h);
+    end
+    
+    disp('la matriz de coeficientes de derivadas es ');disp(coef); 
+
+    du=u*coef %vector de derivda
+    
+    hold all
+    plot(x,du, '**r')  %grafico la derivada de rojo
+    
+    
+    
+    %%INTEGRACIÓN con trapecios compuesto
+    
+    coefint=zeros(N,1);
+    coefint(1)=h*1/2;
+    coefint(N)=h*1/2;
+    
+    for i=2:N-1
+      coefint(i)=h*1;
+    end
+    
+    int=0; % inicializo la variable acumulador
+    for i=1:N
+      int=int+coefint(i)*u(i);
+    end
+
+    disp('El vector de coeficientes de la integral es '),disp(coefint)
+    disp('El valor de la integral es '), disp(int)
+    
 endfunction
